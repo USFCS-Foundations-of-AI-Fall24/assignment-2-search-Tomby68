f08a4c5e62d8f3c992b142e4075be35723aa3c75
Tomby68 <tcsorkin@dons.usfca.edu>
2024-09-22 14:05:25 -0700

    submission.py runs mars_planner, routefinder, and mapcoloring

---
+++ submission.py
@@ -0,0 +1,11 @@
+import mars_planner
+import routefinder
+import mapcoloring
+
+if __name__ == "__main__" :
+    print("Question 2 (mars_planner) output:")
+    mars_planner.main()
+    print("\nQuestion 3 (A*) output:")
+    routefinder.main()
+    print("\nQuestion 4 (mapcoloring) output:")
+    mapcoloring.main()
